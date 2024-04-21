9695cf3a0b465477fb6389178efc3c3872191375
Gustavo Henrique dos Santos Domingos <h.gustavaspeed2002@gmail.com>
2024-04-21 17:33:45 -0300

    add prev pointer to list, delete list struct, add euclidianDistance()

--- atividade-2/atividade1.cpp
+++ atividade-2/atividade1.cpp
@@ -2,7 +2,9 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <GL/gl.h>
+#include <cstdlib>
 #include <stdio.h>
+#include <math.h>
 
 struct Point {
     int x;
@@ -10,78 +12,109 @@ struct Point {
 };
 
 struct Node {
-    Point point; struct Node *next;
+    Point point;
+    struct Node *next;
+    struct Node *prev;
 };
 
-struct {
-    struct Node *head;
-} list;
+// initialize list
+Node *head = NULL;
+Node *tail = NULL;
 
+// initialize point count and point buffer
 int pointCount = 0;
-Point pointBuffer[2];
+Point pointBuffer;
 
-int mouseX = -1;
-int mouseY = -1;
-bool flag = false;
-
+// define window size
 int windowHeight = 320;
 int windowWidth = 320;
 
-void addNewPoint(int x, int y) {
-    Point point;
-    point.x = x;
-    point.y = y;
 
-    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));   
 
+void addPoint(Point point) {
+    struct Node *newNode = (Node*)malloc(sizeof(Node));    
     newNode->next = NULL;
-    newNode->point.x = point.x;
-    newNode->point.y = point.y;
+    newNode->prev = NULL;
+    newNode->point = point;
 
-    printf("new node (%d, %d)\n", newNode->point.x, newNode->point.y);
-
-    if (list.head == NULL) {
-        list.head = newNode;
-        printf("list.head (%d, %d)\n", list.head->point.x, list.head->point.y);
+    if (head == NULL) {
+        head = (Node *) malloc(sizeof(Node));
+        head->point = point;
+        head->next = NULL;
+        head->prev = NULL;
+        tail = (Node *) malloc(sizeof(Node));
+        tail->point = point;
+        tail->next = NULL;
+        tail->prev = NULL;
     } else {
-        Node* current = list.head;
-        while (current != NULL) {
+        Node *current = head;
+        while (current->next != NULL) {
             current = current->next;
         }
-
+        current->next = (Node *) malloc(sizeof(Node));
         current->next = newNode;
+        newNode->prev = (Node *) malloc(sizeof(Node));
+        newNode->prev = current;
+        tail = newNode;
+        
         pointCount++;
-        pointBuffer[pointCount - 1] = current->point;
-        if (pointCount == 2) {
-            pointCount = 0;
-        }
+        pointBuffer = current->point;
+        // printf("Point buffer { x: %d, y: %d}\n", pointBuffer.x, pointBuffer.y);
     }
 }
 
-void displayList() {
-    Node *current = list.head;
-    while (current != NULL) {
-        printf("Display list");
-        printf("x = %d, y = %d", current->point.x, current->point.y);
-        current = current->next;
+double euclidianDistance(int x1, int x2, int y1, int y2) {
+    return sqrt(pow(double(x1 - x2), 2) + pow(double(y1 - y2), 2));
+}
+
+void removePoint(Point point) {
+    if (head == NULL || head->next == NULL) {
+        printf("head or head->next == NULL");
+        return;
     }
+
+}
+
+void displayList() {
+    Node *current = head;
+    int click = 0;
+    printf("[");
+    while (current != NULL) {
+        printf("{ point.x: %d, point.y: %d }, ", current->point.x, current->point.y);
+        current = current->next;
+        click++;
+    }
+    printf("]\n");
+}
+
+void displayListReversed() {
+    Node *current = tail;
+    int click = 0;
+    printf("[");
+    while (current != NULL) {
+        printf("{ point.x: %d, point.y: %d }, ", current->point.x, current->point.y);
+        current = current->prev;
+        click++;
+    }
+    printf("]\n");
 }
 
 void mouseListener(int button, int state, int x, int y) {
     if (state == GLUT_DOWN) {
+        Point point;
+        point.x = x;
+        point.y = glutGet(GLUT_WINDOW_HEIGHT) - y;
         switch (button) {
             case GLUT_LEFT_BUTTON: 
-                mouseX = x;
-                mouseY = glutGet(GLUT_WINDOW_HEIGHT) - y;
-                addNewPoint(mouseX, mouseY);
-                flag = true;
+                printf("left mouse");
+                addPoint(point);
                 glutPostRedisplay();
                 break;
             case GLUT_RIGHT_BUTTON: 
-                // todo   
+                removePoint(point);
+                glutPostRedisplay();
                 break;
-        }                    
-
+        }
     }
 }
 
@@ -91,20 +124,30 @@ void render(void) {
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0, glutGet(GLUT_WINDOW_WIDTH), 0.0, glutGet(GLUT_WINDOW_HEIGHT));
 
-    if (flag) {
-        if (pointCount > 1) {
-            glBegin(GL_LINES);
-                glVertex2f((GLfloat) pointBuffer[0].x, (GLfloat) pointBuffer[0].y);
-                glVertex2f((GLfloat) pointBuffer[1].x, (GLfloat) pointBuffer[1].y);
+    gluOrtho2D(0.0, glutGet(GLUT_WINDOW_WIDTH), 0.0, glutGet(GLUT_WINDOW_HEIGHT));
+    
+    Node *current = head;
+
+    while (current != NULL) { 
+        if (current->next == NULL) {
+            glBegin(GL_POINTS);
+                glVertex2f((GLfloat)current->point.x, (GLfloat)current->point.y);
             glEnd();
+            break;
+        } else {
+            printf("point.x: %d, point.y: %d\t", current->point.x, current->point.y);
+            printf("next.point.x: %d, next.point.y: %d\n", current->next->point.x, current->next->point.y);
+            glBegin(GL_POINTS);
+                glVertex2f((GLfloat)current->point.x, (GLfloat)current->point.y);
+            glEnd();
+            glBegin(GL_LINES);
+                glVertex2f((GLfloat)current->point.x, (GLfloat)current->point.y);
+                glVertex2f((GLfloat)current->next->point.x, (GLfloat)current->next->point.y);
+            glEnd(); 
+            current = current->next;
         }
-        glBegin(GL_POINTS);
-            glVertex2f((GLfloat)mouseX, (GLfloat)mouseY);
-        glEnd();
-        flag = false;
-    }
+    } 
     glFlush();
 }
 
@@ -118,7 +161,7 @@ int main(int argc, char **argv) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glutDisplayFunc(render);
     glutMouseFunc(mouseListener);
-
     glutMainLoop();
+
     return 1;
 }
